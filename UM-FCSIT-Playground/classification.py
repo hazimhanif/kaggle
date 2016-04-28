9365f1febbe545e503840b12c89eb829bdfcf60b
Hazim Hanif <hazimhanif0@gmail.com>
2016-04-28 10:36:35 +0800

    Create classification.py

---
+++ UM-FCSIT-Playground/classification.py
@@ -0,0 +1,27 @@
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+
+training = pd.read_csv(open("C:/Users/Hazim/Downloads/training_set.csv"))
+
+training_data = training.ix[:,0:2]
+training_target = training.ix[:,3]
+
+model = DecisionTreeClassifier()
+model.fit(training_data, training_target)
+
+# make predictions
+#expected = training_target
+predicted = model.predict(training_data)
+   
+# summarize the fit of the model
+#print("\nThe model info : \n")
+#print(metrics.classification_report(expected, predicted))
+#print(metrics.confusion_matrix(expected, predicted))
+   
+#print("The accuracy: ")
+#print(metrics.accuracy_score(expected,predicted))
+
+
+
+
